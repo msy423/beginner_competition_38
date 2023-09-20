3eebdb77ad88eda0515293f82a8aef3d51909ee4
msy423 <k.msy.423@gmail.com>
2023-09-20 12:10:17 +0000

    add some

--- loder/preprocess.py
+++ loder/preprocess.py
@@ -9,16 +9,29 @@ class preprocess_dataloder():
     def __call__(self):
         data = pd.read_csv('data/train.csv')
         data['Gender'] = data['Gender'].map({'Male': 0, 'Female': 1})
-        data['Age_Tbill_ratio'] = data['Age']/data['T_Bil'] 
-        data['Age_Dbill_ratio'] = data['Age']/data['D_Bil'] 
+        data['TP_Tbil_ratio'] = data['TP']/data['T_Bil'] 
+        data['ALTGPT_TP_ratio'] = data['ALT_GPT']/data['TP']
+        
+
+        data['Age_Tbil_ratio'] = data['Age']/data['T_Bil'] 
+        data['Age_Dbil_ratio'] = data['Age']/data['D_Bil']
+        data['Age_ALP_ratio'] = data['Age']/data['ALP']
+        data['Age_ALTGPT_ratio'] = data['Age']/data['ALT_GPT']
+        data['Age_ASTGOT_ratio'] = data['Age']/data['AST_GOT']
+        data['Age_TP_ratio'] = data['Age']/data['TP']
+        data['Age_ALb_ratio'] = data['Age']/data['Alb']
+        data['Age_AG_ratio'] = data['Age']/data['AG_ratio']
+
         data['ALTgpt_ASTgot_diff'] = data['ALT_GPT'] - data['AST_GOT'] 
         data['Tbil_Dbil_diff'] = data['T_Bil'] - data['D_Bil'] 
         data['ALTgpt_ASTgot_prod'] = data['ALT_GPT'] * data['AST_GOT'] 
-        data['Tbil_Dbil_prod'] = data['T_Bil'] * data['D_Bil']
+        data['Tbil_Dbil_prod'] = data['T_Bil'] * data['D_Bil'] 
 
-        data_without_disease=data.drop('disease',axis=1).copy()
-        column_means = data.mean()
-        means_by_disease = data.groupby('disease').mean()
+
+
+        #data_without_disease=data.drop('disease',axis=1).copy()
+        #column_means = data.mean()
+        #means_by_disease = data.groupby('disease').mean()
 
         #print("----------train means----------\n",means_by_disease)
 
@@ -26,7 +39,7 @@ class preprocess_dataloder():
         z_scores = (data - data.mean()) / data.std()
 
         # Zスコアがしきい値を超える行を抽出
-        threshold = 2
+        threshold = 3
         outliers = data[(z_scores > threshold).any(axis=1)]
         # 外れ値を含む行を削除
         cleaned_data = data.drop(outliers.index)
@@ -45,7 +58,7 @@ class preprocess_dataloder():
         '''
         #age, alp, TP Alb,AG_ratioはあまり平均に差がない
 
-        X = cleaned_data.drop('disease', axis=1)  #[['T_Bil', 'D_Bil', 'ALP', 'ALT_GPT', 'AST_GOT', 'TP', 'Alb']]
+        X = cleaned_data.drop(['disease'], axis=1)  #[['T_Bil', 'D_Bil', 'ALP', 'ALT_GPT', 'AST_GOT', 'TP', 'Alb']]
         y = cleaned_data['disease']
 
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
@@ -7,7 +8,10 @@ import lightgbm as lgb
 import xgboost as xgb
 from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression, LinearRegression
+from sklearn.svm import SVC
 from loder.preprocess import preprocess_dataloder
+
 
 pre = preprocess_dataloder()
 X_train, X_test, y_train, y_test, scaler =pre()
@@ -23,13 +27,119 @@ params = {
 'boosting_type': 'gbdt',
 'metric': 'binary_logloss',  # ログ尤度
 'num_leaves': 31,  # 木の最大葉数
-'learning_rate': 0.035,  # 学習率
-'max_bin': 375,
-'num_iterations':3000,
+'learning_rate': 0.01,  # 学習率
+'max_bin': 5000,
+'num_iterations':10000,
+'subsmaple': 0.8,
+'silent':True
 }
 
-model = lgb.train(params, lgb.Dataset(X_train, label=y_train), num_boost_round=100) 
 
+
+
+'''
+
+
+'''
+
+#['Age', 'Gender', 'T_Bil', 'D_Bil', 'ALP', 'ALT_GPT', 'AST_GOT', 'TP',
+#       'Alb', 'AG_ratio', 'TP_Tbil_ratio', 'ALTGPT_TP_ratio', 'Age_Tbil_ratio',
+#       'Age_Dbil_ratio', 'Age_ALP_ratio', 'Age_ALTGPT_ratio',
+#       'Age_ASTGOT_ratio', 'Age_TP_ratio', 'Age_ALb_ratio', 'Age_AG_ratio',
+#       'ALTgpt_ASTgot_diff', 'Tbil_Dbil_diff', 'ALTgpt_ASTgot_prod',
+#       'Tbil_Dbil_prod'],
+
+
+#print(X_train.columns)
+
+gbm_col =[ 'T_Bil', 'D_Bil', 'ALP', 'ALT_GPT','AG_ratio', 'TP_Tbil_ratio',
+          'ALTGPT_TP_ratio', 'Age_Dbil_ratio', 'Age_ALP_ratio', 'Age_ALTGPT_ratio', 
+          'Age_ASTGOT_ratio', 'Age_TP_ratio', 'Age_ALb_ratio', 'Age_AG_ratio', 'ALTgpt_ASTgot_diff','ALTgpt_ASTgot_prod' ]
+
+logi_model = LogisticRegression() 
+svm_model = SVC(kernel='rbf', C=1)
+gbm = lgb.train(params, lgb.Dataset(X_train[gbm_col], label=y_train), num_boost_round=100) 
+
+
+logi_col=['T_Bil', 'D_Bil', 'ALT_GPT', 'Alb',  'TP_Tbil_ratio', 'Age_TP_ratio', 'Age_AG_ratio', 'ALTgpt_ASTgot_prod']
+logi_model.fit(X_train[logi_col],y_train)
+y_pred_logi=logi_model.predict(X_test[logi_col])
+
+svm_col=[ 'AST_GOT', 'TP_Tbil_ratio', 'ALTGPT_TP_ratio', 'Age_ALP_ratio', 'Age_ALTGPT_ratio', 'Age_ASTGOT_ratio', 'Age_TP_ratio', 'Age_AG_ratio', 'Tbil_Dbil_diff']
+svm_model.fit(X_train[svm_col], y_train)
+y_pred_svm = svm_model.predict(X_test[svm_col])
+
+y_pred_gbm = gbm.predict(X_test[gbm_col])
+threshold = 0.5
+y_pred_gbm = (y_pred_gbm > threshold).astype(int)
+
+y_pred = ((y_pred_logi+y_pred_gbm+y_pred_svm)/3 > 0.5).astype(int)
+
+y_preds=pd.DataFrame({'gt':y_test, 'logistic':y_pred_logi, 'SVM':y_pred_svm, 'LightGBM':y_pred_gbm ,'mean':y_pred})
+
+
+y_preds.to_csv('ensamble_experiment_3.csv', header=False, index=True)
+
+
+accuracy = accuracy_score(y_test, y_pred)
+report = classification_report(y_test, y_pred)
+confusion = confusion_matrix(y_test, y_pred)
+
+print(f'Accuracy: {accuracy}')
+print(f'Classification Report:\n{report}')
+print(f'Confusion Matrix:\n{confusion}')
+
+
+
+
+########################################################
+
+print("\n---------------------------Submit set---------------------------\n")
+
+data = pd.read_csv('data/test.csv')
+data['Gender'] = data['Gender'].map({'Male': 0, 'Female': 1})
+column_med = #median 
+data.fillna(column_med, inplace=True)
+
+data['TP_Tbil_ratio'] = data['TP']/data['T_Bil'] 
+data['ALTGPT_TP_ratio'] = data['ALT_GPT']/data['TP']
+
+
+data['Age_Tbil_ratio'] = data['Age']/data['T_Bil'] 
+data['Age_Dbil_ratio'] = data['Age']/data['D_Bil']
+data['Age_ALP_ratio'] = data['Age']/data['ALP']
+data['Age_ALTGPT_ratio'] = data['Age']/data['ALT_GPT']
+data['Age_ASTGOT_ratio'] = data['Age']/data['AST_GOT']
+data['Age_TP_ratio'] = data['Age']/data['TP']
+data['Age_ALb_ratio'] = data['Age']/data['Alb']
+data['Age_AG_ratio'] = data['Age']/data['AG_ratio']
+
+data['ALTgpt_ASTgot_diff'] = data['ALT_GPT'] - data['AST_GOT'] 
+data['Tbil_Dbil_diff'] = data['T_Bil'] - data['D_Bil'] 
+data['ALTgpt_ASTgot_prod'] = data['ALT_GPT'] * data['AST_GOT'] 
+data['Tbil_Dbil_prod'] = data['T_Bil'] * data['D_Bil'] 
+
+means_by_disease_test = test_data.mean()
+print("----------test means----------\n",means_by_disease_test)
+
+
+
+X_test_ = test_data  #test_data[['Gender', 'T_Bil', 'D_Bil', 'ALP', 'ALT_GPT', 'AST_GOT', 'TP', 'Alb']]
+X_test_=scaler.transform(X_test_)
+y_infe = model.predict(X_test_)
+y_infe = (y_infe > threshold).astype(int)
+
+id = [i for i in range(len(y_infe))]
+
+output_data = pd.DataFrame(y_infe)
+output_data.to_csv('predicted_results4.csv', header=False, index=True)
+
+'''
+
+'''
+
+
+######################################################
 '''
 cv_scores = cross_val_score(model, X_train, y_train, cv=5)
 # クロスバリデーションの各分割でのスコアを表示
@@ -88,53 +198,3 @@ print(f'Mean CV Score: {mean_cv_score}')
 
         model = xgb.XGBClassifier(**best_params, random_state=42)
 '''
-
-'''
-'''
-#model.fit(X_train, y_train)
-y_pred = model.predict(X_test)
-
-threshold = 0.3
-y_pred = (y_pred > threshold).astype(int)
-
-accuracy = accuracy_score(y_test, y_pred)
-report = classification_report(y_test, y_pred)
-confusion = confusion_matrix(y_test, y_pred)
-
-print(f'Accuracy: {accuracy}')
-print(f'Classification Report:\n{report}')
-print(f'Confusion Matrix:\n{confusion}')
-
-########################################################
-print("\n---------------------------Submit set---------------------------\n")
-
-test_data = pd.read_csv('data/test.csv')
-test_data['Gender'] = test_data['Gender'].map({'Male': 0, 'Female': 1})
-test_data['Gender'] = test_data['Gender'].map({'Male': 0, 'Female': 1})
-test_data['Age_Tbill_ratio'] = test_data['Age']/test_data['T_Bil'] 
-test_data['Age_Dbill_ratio'] = test_data['Age']/test_data['D_Bil'] 
-test_data['ALTgpt_ASTgot_diff'] = test_data['ALT_GPT'] - test_data['AST_GOT'] 
-test_data['Tbil_Dbil_diff'] = test_data['T_Bil'] - test_data['D_Bil'] 
-test_data['ALTgpt_ASTgot_prod'] = test_data['ALT_GPT'] * test_data['AST_GOT'] 
-test_data['Tbil_Dbil_prod'] = test_data['T_Bil'] * test_data['D_Bil']
-
-means_by_disease_test = test_data.mean()
-print("----------test means----------\n",means_by_disease_test)
-
-test_data.fillna(column_means, inplace=True)
-
-X_test_ = test_data  #test_data[['Gender', 'T_Bil', 'D_Bil', 'ALP', 'ALT_GPT', 'AST_GOT', 'TP', 'Alb']]
-X_test_=scaler.transform(X_test_)
-y_infe = model.predict(X_test_)
-y_infe = (y_infe > threshold).astype(int)
-
-id = [i for i in range(len(y_infe))]
-
-output_data = pd.DataFrame(y_infe)
-output_data.to_csv('predicted_results4.csv', header=False, index=True)
-
-'''
-
-'''
-
-
